d28185692b197303950a9742a5a18aab57649eec
Vaishnav Chincholkar <61238534+thevaishnav@users.noreply.github.com>
2024-11-19 19:11:38 +0530

    Added Interactable Key

---
+++ Bridges/Inventory - Interactable/InteractableKey.cs
@@ -0,0 +1,52 @@
+using InteractionSystem;
+using UnityEngine;
+
+namespace InventorySystem
+{
+    [RequireComponent(typeof(Collider))]
+    public class InteractableKey : InventoryItem, IInteractionProcessor
+    {
+        [SerializeField] private bool _immedietEndInteraction = true;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            if (IsPicked) 
+                gameObject.SetActive(false);
+        }
+
+        void IInteractionProcessor.OnInteractionStart(InteractionPlayer player)
+        {
+            gameObject.SetActive(false);
+            var inventory = player.GetComponent<InventoryPlayer>();
+            if (inventory == null)
+                Debug.LogError($"Cant pickup item as the player does not have \"{nameof(InventoryPlayer)}\" component");
+            else
+                inventory.PickupItem(this);
+            
+            if (_immedietEndInteraction) 
+                player.EndInteraction();
+        }
+
+        void IInteractionProcessor.OnInteractionEnd(InteractionPlayer player)
+        {
+
+        }
+
+        protected override void OnDropped(InventoryPlayer player)
+        {
+
+        }
+
+        protected override void OnPicked(InventoryPlayer player)
+        {
+
+        }
+
+        protected override void OnUse(InventoryPlayer player)
+        {
+
+        }
+    }
+}
---
+++ Bridges/Inventory - Interactable/InteractionRequiresKey.cs
@@ -0,0 +1,46 @@
+﻿using InteractionSystem;
+using UnityEngine;
+
+
+namespace InventorySystem
+{
+    public class InteractionRequiresKey : MonoBehaviour, IInteractionCriteria
+    {
+        private const string COMPONENT = nameof(InventoryPlayer);
+        
+        [SerializeField, Tooltip("If direct referance is possible, use this")] private InventoryItem _key;
+        [SerializeField, Tooltip("If direct referance is not possible, use this")] private string _keyId;
+        [SerializeField] private string _errorMessage;
+
+        private string KeyID
+        {
+            get
+            {
+                if (_key != null) return _key.ID;
+                return _keyId;
+            }
+        }
+
+        private InventoryPlayer _inventoryPlayer;
+
+        bool IInteractionCriteria.CanInteract(InteractionPlayer player)
+        {
+            if (_inventoryPlayer == null || _inventoryPlayer.gameObject != player.gameObject)
+                _inventoryPlayer = player.GetComponent<InventoryPlayer>();
+            if (_inventoryPlayer == null)
+            {
+                Debug.LogError($"Player \"{player.name}\" does not have \"{COMPONENT}\" component");
+                InteractionUiHandler.DisplayError($"Player dont have \"{COMPONENT}\" component");
+                return false;
+            }
+
+            if (!_inventoryPlayer.HasItemInInventory(KeyID))
+            {
+                InteractionUiHandler.DisplayError(_errorMessage);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
